0a4376bc00b49868e941ef406cd1c949bb547d0d
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-06-10 14:06:57 -0500

    Working game loop

---
+++ CGA Jam 5-31 through 6-15/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        Debug.Log("Hit trigger");
+
+        if (collider.tag == "Exit")
+        {
+
+            scoreManager.RecordTime(Time.timeSinceLevelLoad);
+            SceneManager.LoadScene("01_maingame");
+
+
+            Debug.Log("Hit exit collider");
+        }
+    }
+
+
+}
---
+++ CGA Jam 5-31 through 6-15/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    public Text currentTimeDisplay;
+    public Text bestTimeDisplay;
+    private float bestTime;
+    const string PLAYER_PREFS_KEY = "CGAJamBestTime";
+
+    // Use this for initialization
+    void Start()
+    {
+        bestTime = PlayerPrefs.GetFloat(PLAYER_PREFS_KEY, 0f);
+        bestTimeDisplay.text = "Best Time: " + bestTime.ToString("F2");
+
+    }
+    public void RecordTime(float levelTime)
+    {
+        if (levelTime < bestTime) { PlayerPrefs.SetFloat(PLAYER_PREFS_KEY, levelTime); }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimeDisplay.text = Time.timeSinceLevelLoad.ToString("F2");
+    }
+}
